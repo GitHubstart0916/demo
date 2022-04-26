8bbab8dbcbd7f084799cb0d5820a108eace43606
刘传 <18375299@buaa.edu.cn>
2022-04-26 14:55:56 +0800

    clear

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "CI Clear",
+			"message": "CI Clear !!!",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
