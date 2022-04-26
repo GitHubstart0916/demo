536472e5fac94fad4857fc4c435e235affe6948a
刘传 <18375299@buaa.edu.cn>
2022-04-26 14:40:12 +0800

    test

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong123123",
+			"message": "pong123",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
